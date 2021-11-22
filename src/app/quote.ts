4fb9225032d2f26fe93cb91693daed8ac811e052
AdamuWanganya <wanganyaadamu@gmail.com>
2021-11-22 07:41:22 +0300

    Add author to the quote blueprint

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,6 @@
 export class Quote {
   id:number;
   name:String;
+  author:String;
 }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {id:1, name:'Live as if you were to die tomorrow. Learn as if you were to live forever.'},
-    {id:2, name: 'Be yourself; everyone else is already taken.'},
-    {id:3, name: 'Be the change that you wish to see in the world.'},
-    {id:4, name: 'Learn as if you were to live forever.'},
-    {id:5, name: 'We accept the love we think we deserve.'},
-    {id:6, name: 'The opposite of love is not hate, it is indifference.'},
+    {id:1, name:'Live as if you were to die tomorrow. Learn as if you were to live forever.',author:'- Mahatma Gandhi'},
+    {id:2, name: 'Be yourself; everyone else is already taken.',author:'― Oscar Wilde'},
+    {id:3, name: 'Be the change that you wish to see in the world.',author:'― Mahatma Gandhi'},
+    {id:4, name: 'Learn as if you were to live forever.',author:'― Mahatma Gandhi'},
+    {id:5, name: 'We accept the love we think we deserve.',author:'- inspirational love'},
+    {id:6, name: 'The opposite of love is not hate, it is indifference.',author:'― Elie Wiesel'},
 
   ];
  

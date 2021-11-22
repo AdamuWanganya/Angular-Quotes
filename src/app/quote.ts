165bb34049fd3c284eb8386d8697748e9e3eb8ed
AdamuWanganya <wanganyaadamu@gmail.com>
2021-11-23 00:26:53 +0300

    Add the submit button

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
  public showAuthor: boolean;
-  constructor(public id: number,public name: String,public author: String,public completeDate: Date){
+  constructor(public id: number,public name: String,public author: String,public post:String,public completeDate: Date){
     this.showAuthor =false;
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,14 +9,21 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Live as if you were to die tomorrow. Learn as if you were to live forever.','- Mahatma Gandhi',new Date(2012,7,12)),
-    new Quote(2, 'Be yourself; everyone else is already taken.','― Oscar Wilde',new Date(2013,7,12)),
-    new Quote(3, 'Be the change that you wish to see in the world.','― Mahatma Gandhi',new Date(2018,7,12)),
-    new Quote(4, 'Learn as if you were to live forever.','― Mahatma Gandhi',new Date(2014,7,12)),
-    new Quote(5, 'We accept the love we think we deserve.','- inspirational love',new Date(2020,7,12)),
-    new Quote(6, 'The opposite of love is not hate, it is indifference.','― Elie Wiesel',new Date(2015,1,13)),
+    new Quote(1, 'Live as if you were to die tomorrow. Learn as if you were to live forever.','- Mahatma Gandhi','Hassan',new Date(2012,7,12)),
+    new Quote(2, 'Be yourself; everyone else is already taken.','― Oscar Wilde','Cindy',new Date(2013,7,12)),
+    new Quote(3, 'Be the change that you wish to see in the world.','― Mahatma Gandhi','Adamu',new Date(2018,7,12)),
+    new Quote(4, 'Learn as if you were to live forever.','― Mahatma Gandhi','Everyline',new Date(2014,7,12)),
+    new Quote(5, 'We accept the love we think we deserve.','- inspirational love','Cosma',new Date(2020,7,12)),
+    new Quote(6, 'The opposite of love is not hate, it is indifference.','― Elie Wiesel','Mohammed',new Date(2015,1,13)),
 
   ];
+
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
 
   completeQuote(isComplete, index){
     if (isComplete){
